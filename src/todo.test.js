52d78e9f1117394f30296a31f9db34861f7087e6
Diego <diegodsha@hotmail.com>
2021-06-11 11:01:04 -0500

    Add todo module test

---
+++ src/todo.test.js
@@ -0,0 +1,19 @@
+import Todo from './todo';
+
+const todo1 = new Todo();
+it('returns an object of class todo', () => {
+  expect(todo1).toBeInstanceOf(Todo);
+});
+
+it('returns an object of class todo', () => {
+  todo1.id = 1;
+  todo1.title = 'todoNAme';
+  todo1.description = 'todoDescription';
+  todo1.dueDate = '06/10/2021';
+  todo1.priority = 'medium';
+  expect(todo1.id).toBe(1);
+  expect(todo1.title).toBe('todoNAme');
+  expect(todo1.description).toBe('todoDescription');
+  expect(todo1.dueDate).toBe('06/10/2021');
+  expect(todo1.priority).toBe('medium');
+});
